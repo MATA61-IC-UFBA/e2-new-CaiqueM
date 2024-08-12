6b15d351477465dc498e86b6cdb8cb9e3316c4ab
Caíque Moura <41167585+CaiqueM@users.noreply.github.com>
2024-08-12 17:18:20 -0300

    atividade 2 submitted

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,30 @@
 /* main.c */
+#include <stdio.h>
+#include "token.h"
+extern int yylex();
+extern int yyval;
 
+int main(){
+    int token;
+    while((token == yylex())){
+        switch (token){
+            case NUMBER;
+                printf("constante decimal: %d \n", yyval);
+                break;
+            
+            
+            case PLUS:
+
+            case  MINUS:
+
+            case TIMES:
+
+            case DIV:
+                printf("codigo de token : %d \n",token);
+                break;
+            default:
+                printf("codigo de token: %d \n",token)
+        }
+    }
+    return 0;
+}
